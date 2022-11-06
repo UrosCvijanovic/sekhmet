863a5c61cff511d2f92c470a08fa75f2e1da9ebf
Uros Cvijanovic <uros.cvijanovic@gmail.com>
2022-11-06 12:41:42 +0800

    Bootstrapping the application with hibernate

---
+++ src/main/java/com/sekhmet/SekhmetApplication.java
@@ -0,0 +1,13 @@
+package com.sekhmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekhmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekhmetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sekhmet/models/User.java
@@ -0,0 +1,27 @@
+package com.sekhmet.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
